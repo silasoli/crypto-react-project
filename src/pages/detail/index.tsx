da785d9f473f3afa310e566ef3392b65267ade4c
Silasoli <silas.oliveira@blockhub.builders>
2023-07-22 21:17:05 -0300

    feat: adding start of project

---
+++ src/pages/detail/index.tsx
@@ -0,0 +1,7 @@
+export function Detail() {
+  return (
+    <div>
+      <h1>Pagina Detalhes</h1>
+    </div>
+  );
+}
---
+++ src/pages/notfound/index.tsx
@@ -0,0 +1,7 @@
+export function NotFound() {
+  return (
+    <div>
+      <h1>Pagina 404 não existe</h1>
+    </div>
+  );
+}
